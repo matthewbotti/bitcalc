a31614745b2f93f1317d779d3aa32ea57743c8a6
Matthew Botti <matthewbotti@protonmail.com>
2020-07-06 02:29:27 -0400

    Add USD conversion formulas

--- bitcalc.py
+++ bitcalc.py
@@ -2,6 +2,7 @@ import requests
 import click
 
 UNIT_CHOICES = ['BTC', 'mBTC', 'uBTC', 'SAT', 'mSAT']
+FIAT_CHOICES = ['USD']
 UNITS = {'BTC':1, 'mBTC':0.001, 'uBTC':0.000001, 'SAT':0.00000001, 'mSAT':0.00000000001}
 
 # Fetch current Bitcoin price from Coindesk api
@@ -14,11 +15,22 @@ def btc_price():
 
 @click.command()
 @click.argument('start_amt', type=float)
-@click.argument('start_unit', type=click.Choice(UNIT_CHOICES, case_sensitive=False))
-@click.argument('end_unit', type=click.Choice(UNIT_CHOICES, case_sensitive=False))
+@click.argument('start_unit', type=click.Choice(UNIT_CHOICES+FIAT_CHOICES, case_sensitive=False))
+@click.argument('end_unit', type=click.Choice(UNIT_CHOICES+FIAT_CHOICES, case_sensitive=False))
 def cli(start_amt, start_unit, end_unit):
-    """Convert start_amt between different Bitcoin unit types"""
+    """Convert start_amt between different Bitcoin & fiat unit types"""
 
-    end_amt = (UNITS[start_unit] / UNITS[end_unit]) * start_amt
+    # convert from a bitcoin unit to a fiat unit
+    if end_unit in FIAT_CHOICES:
+        end_amt = start_amt * UNITS[start_unit] * btc_price()    
+        click.echo(f'{end_amt:,.2f} {end_unit}')
 
-    click.echo(f'{end_amt:,} {end_unit}')    
+    # convert from a fiat unit to a bitcoin unit
+    elif start_unit in FIAT_CHOICES:
+        end_amt = start_amt / btc_price() / UNITS[end_unit] 
+        click.echo(f'{end_amt:,.4f} {end_unit}')
+
+    # convert from one bitcoin unit to another bitcoin unit
+    else:
+        end_amt = (UNITS[start_unit] / UNITS[end_unit]) * start_amt
+        click.echo(f'{end_amt:,} {end_unit}')    
